39fe2cd4875ac89cb045ccd46397af64416de6c9
tanya <tanya.verma@omnenest.com>
2026-02-23 15:09:40 +0530

    pushed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import { useState } from 'react';
  
 // Data
 import { stocks, trades } from './data/stockData';
